634306e2993ddc87eed18fab871ea49f895fbabf
Daijour Williams <90521129+daijourw@users.noreply.github.com>
2024-12-01 23:58:51 +0000

    reset files from template

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import TestComponent from "./components/TestComponent";
 
-const Comp = () => {
-  return <p>This is a comp component</p>
-}
 
 function App() {
   return (
-    <div className="App" id='root'>
-            <TestComponent />
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Comp />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">
+      <h1>Welcome to the Color Game</h1>
     </div>
   );
 }
