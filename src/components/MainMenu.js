90c18648baa85e0f81053594fccc30c59d69bcd0
Daijour Williams <90521129+daijourw@users.noreply.github.com>
2024-12-05 19:10:36 +0000

    a

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -4,6 +4,26 @@ const MainMenu = ({ username, setUsername, leaderboard, onStart }) => {
   return (
     <div className="main-menu">
       <h1>Color Game</h1>
+
+      <div style={{ padding: '10px', fontSize: '18px', color: '#333', maxWidth: '60%', margin: 'auto',}}>
+        <h3>How to Play:</h3>
+        <p>
+          The Color Game tests your reflexes and ability to distinguish different colors. On the
+          first round, you will start with <strong>3 seconds</strong>, and a grid of colors will
+          display.
+        </p>
+        <p>
+          Only one square in the grid will be a <strong>slightly different color</strong>. Choose
+          the odd square that is a different color to gain extra time, a point, and advance to the
+          next round.
+        </p>
+        <p>
+          Each round will have a <strong>larger grid</strong> and a <strong>less distinguishable</strong> color. But be careful! If you choose the wrong square or run out of time, the game will be over.
+        </p>
+        <p>Press the play button to start!</p>
+      </div>
+
+
       <div className="leaderboard">
         <h2>Leaderboard</h2>
         <ul>
